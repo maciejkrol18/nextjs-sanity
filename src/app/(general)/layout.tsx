efd0bd49ac89b28507548d628f208ee9d86c8667
Maciej Król <maciekgplay@gmail.com>
2023-07-26 17:22:50 +0200

    Header colors

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -6,11 +6,11 @@ const GeneralLayout = ({ children }: { children: React.ReactNode }) => {
       <header className="px-4 container mx-auto">
         <div className="flex items-center justify-between py-2">
           <div className="flex gap-2">
-            <PlugZap className="w-6 h-6" />
-            <p className="text-lg">Fotowoltaika Śliwson</p>
+            <PlugZap className="text-slate-900 w-6 h-6" />
+            <p className="text-lg text-slate-800">Fotowoltaika Śliwson</p>
           </div>
           <div>
-            <Menu className="w-6 h-6" />
+            <Menu className="text-slate-900 w-6 h-6" />
           </div>
         </div>
       </header>
